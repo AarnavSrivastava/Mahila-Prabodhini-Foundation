3f1aa30044d189276c1a6ab44cc31c60d73cc0f9
Aarnav Srivastava <aarnav.srivastava18@gmail.com>
2022-09-03 16:15:18 -0400

    Changes to navbar

--- mph-website/src/components/navbar.js
+++ mph-website/src/components/navbar.js
@@ -5,10 +5,10 @@ import Logo from './logo.svg';
 const Navbar = () =>
 {
     return (
-        <nav className="flex flex-row w-screen sticky top-0 z-50 justify-between items-center bg-white-transparent py-[1%] px-[5%] gap-[10%]">
+        <nav className="flex flex-row w-screen sticky top-0 z-50 justify-between items-center bg-white-transparent py-[1.5%] px-[5%] gap-[5%]">
             <Link to="/" className ="flex group flex-row justify-start items-center basis-0 grow gap-[0.5vw]">
                 <img className="group-hover:animate-spin w-[8%]" src={Logo} id="logo" alt="logo"/>
-                <div className="flex flex-row group font-lato font-bold text-[1.1vw]">
+                <div className="flex flex-row group font-lato font-bold text-[1.8vw]">
                     <span className="transition ease-in-out duration-[150ms] group-hover:text-defpink">M</span>
                     <span className="transition ease-in-out duration-[200ms] group-hover:text-defpink">a</span>
                     <span className="transition ease-in-out duration-[250ms] group-hover:text-defpink">h</span>
@@ -45,15 +45,15 @@ const Navbar = () =>
                 <CustomLink to="/contactus">Contact Us</CustomLink>
             </ul>
 
-            <ul className="flex basis-0 grow flex-row gap-5 font-opensans font-bold text-[0.9vw]">
+            <ul className="flex basis-0 grow flex-row gap-5 font-opensans font-bold text-[1.2vw] h-[2.8vw]">
             {/* flex w-[100%] h-[2.5vw] justify-center items-center border-2 border-defpink rounded-md text-white bg-defpink hover:text-defpink hover:bg-white */}
-                <Link className="flex w-[100%] h-[2.5vw] justify-center items-center transition rounded-md text-white border-2 border-defpink ease-in-out delay-10 bg-defpink hover:bg-white hover:text-defpink duration-300" to="/">
+                <Link className="flex px-[1vw] w-[100%] justify-center items-center transition rounded-md text-white border-2 border-defpink ease-in-out delay-10 bg-defpink hover:bg-white hover:text-defpink duration-300" to="/login">
                     LOGIN
                 </Link>
-                <Link className="flex w-[100%] h-[2.5vw] justify-center items-center transition rounded-md text-defpink border-2 border-defpink ease-in-out delay-10 bg-white hover:bg-defpink hover:text-white duration-300" to="/signup">
+                <Link className="flex px-[1vw] w-[100%] justify-center items-center transition rounded-md text-defpink border-2 border-defpink ease-in-out delay-10 bg-white hover:bg-defpink hover:text-white duration-300" to="/signup">
                     SIGN UP
                 </Link>
-                <Link className="flex w-[100%] h-[2.5vw] justify-center items-center transition rounded-md border-2 border-[#158ba8] rounded-md text-[#158ba8] ease-in-out delay-10 bg-white hover:text-white hover:bg-[#158ba8] duration-300" to="/donate">
+                <Link className="flex px-[1vw] w-[100%] justify-center items-center transition rounded-md border-2 border-[#158ba8] rounded-md text-[#158ba8] ease-in-out delay-10 bg-white hover:text-white hover:bg-[#158ba8] duration-300" to="/donate">
                     DONATE
                 </Link>
             </ul>
@@ -66,7 +66,7 @@ function CustomLink({ to, children })
     const resolvedPath = useResolvedPath(to)
     const isActive = useMatch({ path: resolvedPath.pathname, end: true})
     return (
-        <li className="flex font-lato font-black text-[1.1vw]">
+        <li className="flex font-lato font-black text-[1.4vw]">
             <Link to={to} className={"transition ease-in-out duration-150 hover:text-pink-400 hover:font-medium " + (isActive ? "text-defpink" : "") + " duration-300"}>{children}</Link>
         </li>
     )
