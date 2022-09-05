397a41d83e029afa4a30b37eb25057800c467f39
Aarnav Srivastava <aarnav.srivastava18@gmail.com>
2022-09-04 22:38:10 -0400

    Changed navbar

--- mph-website/src/components/navbar.js
+++ mph-website/src/components/navbar.js
@@ -5,9 +5,9 @@ import Logo from './logo.svg';
 const Navbar = () =>
 {
     return (
-        <nav className="flex flex-row w-screen sticky top-0 z-50 justify-between items-center bg-white-transparent py-[1.5%] px-[5%] gap-[5%]">
-            <Link to="/" className ="flex group flex-row justify-start items-center basis-0 grow gap-[0.5vw]">
-                <img className="group-hover:animate-spin w-[8%]" src={Logo} id="logo" alt="logo"/>
+        <nav className="flex flex-row w-screen sticky top-0 z-50 justify-between items-center bg-white-transparent px-[5%] gap-[9%]">
+            <Link to="/" className ="flex group flex-row justify-start items-center basis-0 grow gap-[0.5vw] my-[2%]">
+                <img className="group-hover:animate-spin w-[3vw]" src={Logo} id="logo" alt="logo"/>
                 <div className="flex flex-row group font-lato font-bold text-[1.8vw]">
                     <span className="transition ease-in-out duration-[150ms] group-hover:text-defpink">M</span>
                     <span className="transition ease-in-out duration-[200ms] group-hover:text-defpink">a</span>
@@ -39,24 +39,25 @@ const Navbar = () =>
                     <span className="transition ease-in-out duration-[1400ms] group-hover:text-defpink">n</span>
                 </div>
             </Link>
-            <ul className="basis-0 grow flex justify-center flex-row gap-[1.3vw]">
+            <ul className="hidden md:flex basis-0 grow justify-center flex-row gap-[1.3vw] my-[2%]">
                 <CustomLink to="/aboutus">About Us</CustomLink>
                 <CustomLink to="/events">Events</CustomLink>
                 <CustomLink to="/contactus">Contact Us</CustomLink>
             </ul>
 
-            <ul className="flex basis-0 grow flex-row gap-5 font-opensans font-bold text-[1.2vw] h-[2.8vw]">
+            <ul className="hidden md:flex basis-0 grow flex-row gap-5 font-opensans font-bold text-[1vw] my-[2%]">
             {/* flex w-[100%] h-[2.5vw] justify-center items-center border-2 border-defpink rounded-md text-white bg-defpink hover:text-defpink hover:bg-white */}
-                <Link className="flex px-[1vw] w-[100%] justify-center items-center transition rounded-md text-white border-2 border-defpink ease-in-out delay-10 bg-defpink hover:bg-white hover:text-defpink duration-300" to="/login">
+                <Link className="flex px-[1vw] py-[0.5vw] w-[100%] justify-center items-center transition rounded-md text-white border-2 border-defpink ease-in-out delay-10 bg-defpink hover:bg-white hover:text-defpink duration-300" to="/login">
                     LOGIN
                 </Link>
-                <Link className="flex px-[1vw] w-[100%] justify-center items-center transition rounded-md text-defpink border-2 border-defpink ease-in-out delay-10 bg-white hover:bg-defpink hover:text-white duration-300" to="/signup">
+                <Link className="flex px-[1vw] py-[0.5vw] w-[100%] justify-center items-center transition rounded-md text-defpink border-2 border-defpink ease-in-out delay-10 bg-white hover:bg-defpink hover:text-white duration-300" to="/signup">
                     SIGN UP
                 </Link>
-                <Link className="flex px-[1vw] w-[100%] justify-center items-center transition rounded-md border-2 border-[#158ba8] rounded-md text-[#158ba8] ease-in-out delay-10 bg-white hover:text-white hover:bg-[#158ba8] duration-300" to="/donate">
+                <Link className="flex px-[1vw] py-[0.5vw] w-[100%] justify-center items-center transition rounded-md border-2 border-[#158ba8] rounded-md text-[#158ba8] ease-in-out delay-10 bg-white hover:text-white hover:bg-[#158ba8] duration-300" to="/donate">
                     DONATE
                 </Link>
             </ul>
+            
         </nav>
     )
 }
